f3968b84f27b8cf10669f69a7910a670f7f843c2
032 - Abhishek Choksi <19bmiit032@gmail.com>
2022-02-17 12:14:51 +0530

    Tab Layout Application @ 2022 #032AbhishekChoksi. All rights reserved.

---
+++ app/src/main/java/com/abhishekchoksi/tablayoutapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.abhishekchoksi.tablayoutapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabChats, tabStatus, tabCalls;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabBar);
+        tabChats = findViewById(R.id.tabChats);
+        tabStatus= findViewById(R.id.tabStatus);
+        tabCalls = findViewById(R.id.tabCalls);
+        viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        // one last step is to change the tabs view when the tab is selected or clicked
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
